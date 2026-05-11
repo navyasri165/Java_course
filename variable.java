7efbeeebbd450f33e166664e525b94972ab9009c
navyasri165 <cnavyasri442@gmail.com>
2026-05-11 19:28:16 +0530

    Add files via upload

---
+++ variable.java
@@ -0,0 +1,9 @@
+public class variable {
+    public static void main(String[]args)
+    {
+        int num1=1;
+        int num2=7;
+        int res=num1+num2;
+        System.out.println(res);
+    }
+}
