7cd3205880870f792472b36bec11ee6faa6f7c82
navyasri165 <cnavyasri442@gmail.com>
2026-05-11 19:49:17 +0530

    Add files via upload

---
+++ datatytpes.java
@@ -0,0 +1,18 @@
+public class datatytpes
+{
+    public static void main(String[]args){
+        int a=7;
+        byte b=127;
+        short s=35;
+        long l=3648578777l;
+        float f=77.7777f;
+        double d=777.77777777777777;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+
+    }
+}
