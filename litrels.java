56f8b40d4994f064cabd01bfc25612fe31947933
navyasri165 <cnavyasri442@gmail.com>
2026-05-11 20:19:06 +0530

    Add files via upload

---
+++ litrels.java
@@ -0,0 +1,23 @@
+public class litrels {
+    public static void main(String[] args) {
+        int num1 = 777;
+        int num2 = 0B1001;
+        int num3 = 0XA45D;
+        float f = 12.000f;
+        int num4 = 10_00_00_000;
+        double s = 34e76;
+        double d = 12345.4444;
+        char c = 'n';
+        c++;
+        System.out.println(c);
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(f);
+        System.out.println(num4);
+        System.out.println(s);
+        System.out.println(d);
+    }
+}
+
+
+
