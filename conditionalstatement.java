11de3c3d7c93cbeb15a1ec35da608b12c1922064
navyasri165 <cnavyasri442@gmail.com>
2026-05-12 17:07:24 +0530

    Add files via upload

---
+++ conditionalstatement.java
@@ -0,0 +1,12 @@
+class conditionalstatement{
+    public static void main(String args[]){
+        int x=30;
+        int y=45;
+        if(x>y){
+        System.out.println("y");
+        }
+        else{
+        System.out.println(x);
+        }
+    }
+}
