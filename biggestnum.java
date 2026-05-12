0b2466b68615d301b33dfeec692121a2bf752dab
navyasri165 <cnavyasri442@gmail.com>
2026-05-12 18:14:35 +0530

    Add files via upload

---
+++ biggestnum.java
@@ -0,0 +1,18 @@
+class biggestnum{
+    public static void main(String[]args){
+        int a=10;
+        int b=40;
+        int c=9;
+        if((a>b)&&(a>c)){
+        System.out.println(a);
+        }
+        else if((b>c)&&(b>a)){
+        System.out.println(b);
+     }
+        else{
+            System.out.println(c);
+        }
+
+
+    }
+}
