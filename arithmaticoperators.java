332ee143fc36eeff01c61465aa2666bb1cf932ff
navyasri165 <cnavyasri442@gmail.com>
2026-05-12 13:45:41 +0530

    Add files via upload

---
+++ arithmaticoperators.java
@@ -0,0 +1,18 @@
+class arithmaticoperators{
+    public static void main(String args[]){
+        int a=7;
+        int b=14;
+        int res =a+b;
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+        System.out.println(a-b);
+        System.out.println(res);
+        a++;
+        System.out.println(a);
+        ++a;
+        System.out.println(a);
+
+    }
+
+}
