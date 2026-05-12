37958e4c3d680b1aab1d05b92d13e9e15e11d09f
navyasri165 <cnavyasri442@gmail.com>
2026-05-12 13:16:33 +0530

    Add files via upload

---
+++ typeconversion.java
@@ -0,0 +1,10 @@
+public class typeconversion {
+    public static void main(String[] args) {
+        byte b = 125;
+        int a = (int)b;
+        float t = 56.8f;
+        int s = (int)t;
+        System.out.println(s);
+        System.out.println(a);
+    }
+}
