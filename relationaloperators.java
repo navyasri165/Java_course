b3c01e84be9f14e65fd59603beff898f32e52208
navyasri165 <cnavyasri442@gmail.com>
2026-05-12 14:14:04 +0530

    Add files via upload

---
+++ relationaloperators.java
@@ -0,0 +1,14 @@
+class relationaloperators{
+    public static void main(String[]args){
+        int a=25;
+        int b=36;
+        boolean res=a<b;
+        System.out.println(res);
+        System.out.println(a>b);
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        System.out.println(a>=b);
+        System.out.println(a<=b);
+
+    }
+}
