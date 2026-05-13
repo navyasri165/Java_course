b8fa8e4d11da367288fa2a125dc704957be9a9c9
navyasri165 <cnavyasri442@gmail.com>
2026-05-13 17:11:37 +0530

    Add files via upload

---
+++ creationarray.java
@@ -0,0 +1,12 @@
+public class creationarray {
+    public static void main(String[] args) {
+        int nums[] = new int[4];
+        nums[0] = 2;
+        nums[1] = 5;
+        nums[2] = 4;
+        nums[3] = 9;
+        for (int i = 0; i < 4; i++) {
+            System.out.println(nums[i]);
+        }
+    }
+}
