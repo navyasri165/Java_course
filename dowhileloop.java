7a9a8f753bd207c986fbff69ce0aabd1dceb022a
navyasri165 <cnavyasri442@gmail.com>
2026-05-13 08:22:51 +0530

    Add files via upload

---
+++ dowhileloop.java
@@ -0,0 +1,9 @@
+public class dowhileloop {
+    public static void main(String[]args){
+        int i=1;
+        do{
+            System.out.println("hi" + i);
+            i++;
+        }while(i<=5);
+    }
+}
