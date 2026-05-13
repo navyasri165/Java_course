064046010a493d38751a7990fcf403022318bc7a
navyasri165 <cnavyasri442@gmail.com>
2026-05-13 09:02:14 +0530

    Add files via upload

---
+++ forloop.java
@@ -0,0 +1,14 @@
+public class forloop {
+    public static void main(String args[]){
+        int i=0;
+        int j=1;
+        for(i=0;i<=5;i++){
+            System.out.println("day" + i);
+           for (j=0;j<5;j++){
+               System.out.println("9 am" +j);
+               j++;
+           }
+
+        }
+    }
+}
